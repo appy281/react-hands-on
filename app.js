840fd516e03bf7f7e338c087db827a02371bf896
apurvadhakre22 <apurva.dhakre@piie.ai>
2023-11-05 03:09:30 +0530

    food ordering website basic setup

--- app.js
+++ app.js
@@ -1,6 +1,67 @@
- import React from "react";
- import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import './app.css'
 
- const heading = React.createElement("h1",{},"Hello world from React!");
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(heading);
+const Header = () => {
+ return(<div className="hdr-cont">
+    <div className="hdr-logo">
+        <img src="https://img.freepik.com/free-vector/restaurant-logo-editorial-template_23-2148701251.jpg"></img>
+    </div>
+    <div className="hdr-nav-cont">
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+
+        </ul>
+    </div>
+ </div>);
+};
+
+// eslint-disable-next-line react/prop-types
+const RestaurantCard = ({label}) => {
+return <div className="res-crd-cont">
+        <image className="res-crd-img"></image>
+        <div className="res-crd-lbl">{label}</div>
+        <div className="res-crd-rtg">{label}</div>
+        <div className="res-crd-cns">{label}</div>
+    </div>
+}
+
+// eslint-disable-next-line react/prop-types
+const Search = () => {
+    return <div className="search-cont">
+            <input type="search"className="search-bx"></input>
+           </div>
+}
+
+const Body = () => {
+    return(<div className="bdy-cont">
+        <Search/>
+      <div className="ress-cont">
+        <RestaurantCard label={"Haldiram's"}/>
+        <RestaurantCard label={"Koji"}/>
+        <RestaurantCard label={"Whispering Bamboo"}/>
+        <RestaurantCard label={"Haldiram's"}/>
+        <RestaurantCard label={"Koji"}/>
+        <RestaurantCard label={"Whispering Bamboo"}/>
+        <RestaurantCard label={"Haldiram's"}/>
+        <RestaurantCard label={"Koji"}/>
+        <RestaurantCard label={"Whispering Bamboo"}/>
+      </div>
+
+    </div>);
+   };
+
+const MainLayout = () => {
+    return(
+        <div className="mn-lyt">
+            <Header/>
+            <Body/>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MainLayout/>);
